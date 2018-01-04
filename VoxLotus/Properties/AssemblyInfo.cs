c381e16ea4c90260f274c71b3870a84898904af8
Arsonide <arsonide@yahoo.com>
2018-01-04 17:28:54 -0600

    Update Assembly Info as well.

--- VoxLotus/Properties/AssemblyInfo.cs
+++ VoxLotus/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VoxLotus")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Vox Lotus")]
+[assembly: AssemblyDescription("A system tray application for the game Warframe that informs players about in-game events.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VoxLotus")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCompany("Arsonide")]
+[assembly: AssemblyProduct("Vox Lotus")]
+[assembly: AssemblyCopyright("Copyright 2018 © Arsonide. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.12.0.0")]
+[assembly: AssemblyFileVersion("0.12.0.0")]
