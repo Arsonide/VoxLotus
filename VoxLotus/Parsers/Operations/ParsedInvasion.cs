6e8eb590f100c8d52355d863c651f89a380bfacd
Arsonide <arsonide@yahoo.com>
2018-01-10 09:22:02 -0600

    ParsedInvasions will attempt to provide an estimate of the time remaining on their invasions using current completion and starting time.

--- VoxLotus/Parsers/Operations/ParsedInvasion.cs
+++ VoxLotus/Parsers/Operations/ParsedInvasion.cs
@@ -31,6 +31,31 @@ namespace VoxLotus.Parsers
             }
         }
 
+        public bool TryGetETA(out string eta)
+        {
+            if (invasion == null)
+            {
+                eta = string.Empty;
+                return false;
+            }
+
+            double completion = Completion;
+            double minutes = DateTime.UtcNow.Subtract(invasion.StartTime).TotalMinutes;
+
+            // If completion or time elapsed is too low, the estimate won't be very good.
+            if (completion < 5 || minutes < 5)
+            {
+                eta = string.Empty;
+                return false;
+            }
+
+            double completionRate = completion / minutes;
+            double completionLeft = 100 - completion;
+
+            eta = Utilities.ReadableTimeSpan(TimeSpan.FromMinutes(completionLeft / completionRate), false);
+            return true;
+        }
+
         public override string Description(DescriptionType type)
         {
             switch(type)
@@ -40,7 +65,7 @@ namespace VoxLotus.Parsers
                 case DescriptionType.Logged:
                     return $"{invasion.Node}: {invasion.Description}. Rewards: {reward.Description(false, true, false)}.";
                 case DescriptionType.Progress:
-                    return $"Completion: {Math.Round(Completion)}%.";
+                    return TryGetETA(out string eta) ? $"Completion: {Math.Round(Completion)}%. Estimated Remaining: {eta}." : $"Completion: {Math.Round(Completion)}%.";
                 default:
                     return $"{invasion.Node}: {invasion.Description}. Rewards: {reward.Description(false, true, false)}.";
 
